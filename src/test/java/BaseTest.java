b4af834082a661e0e110756a3e7ec8e729876b93
Владимир <telemetristika@gmail.com>
2026-02-16 17:55:49 +0300

    Version_1.2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class BaseTest {
+public abstract class BaseTest {
     protected WebDriver driver;
     protected Logger logger;
 
